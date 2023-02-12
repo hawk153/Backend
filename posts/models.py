bd20a12a7e43f873750504fa12ae79e41fe0fcde
hawk153 <hawk153@me.com>
2023-02-12 19:48:19 +0300

    Backend

---
+++ posts/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+from ckeditor_uploader.fields import RichTextUploadingField
+
+
+class Author(models.Model):
+    class Meta:
+        verbose_name = 'Автор'
+        verbose_name_plural = 'Авторы'
+
+    name = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Автор')
+    is_subscriber = models.BooleanField(verbose_name='Подписчик', default=False)
+
+    def __str__(self):
+        return f'{self.name.username}'
+
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
+
+    author = models.ForeignKey('Author', on_delete=models.CASCADE, verbose_name='Автор')
+    title = models.CharField(max_length=128, verbose_name='Заголовок')
+    content = RichTextUploadingField(verbose_name='Объявление')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def get_absolute_url(self):
+        return f'posts/{self.pk}'
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    categories = [('cards', 'Карты'),
+                  ('decks', 'Колоды'),
+                  ('heroes', 'Герои'),
+                  ('tactic', 'Тактика'),
+                  ('strategy', 'Стратегия'),
+                  ('mage', 'Маг'),
+                  ('warlock', 'Чернокнижник'),
+                  ('paladin', 'Паладин'),
+                  ]
+
+    name = models.CharField(max_length=8, choices=categories, verbose_name='Категория', default='tank', unique=True)
+
+    def __str__(self):
+        return self.get_name_display()
+
+
+class Reply(models.Model):
+    class Meta:
+        verbose_name = 'Отклик'
+        verbose_name_plural = 'Отклики'
+
+    author = models.ForeignKey('Author', on_delete=models.CASCADE, verbose_name='Автор')
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, verbose_name='Объявление')
+    content = models.TextField(verbose_name='Отклик')
+    created = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.content[0:32]}'
